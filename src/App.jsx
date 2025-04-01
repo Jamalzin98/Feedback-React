5eb6c155631a0524670e97219352202f7bfe70f6
gabriel.oliverpenha@gmail.com <gabriel.oliverpenha@gmail.com>
2025-03-31 22:01:30 -0300

    Correção de bug2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,60 @@
+import { useState } from "react"
 import iconStar from "./assets/icon-star.svg"
 import illustrationThankYou from "./assets/illustration-thank-you.svg"
-import {useState} from "react"
 
 export function App() {
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
-  const [submited, setSubmited] = userState(false)
+  const [submited, setSubmited] = useState(false)
 
-  function handleMudarNotaAvaliacao(nota){
+  function handleAlterarNotaAvaliacao(nota) {
     setNotaAvaliacao(nota)
 
+    console.log(nota)
   }
 
-  function handleSubmit () {
+  function handleSubmit() {
     if (notaAvaliacao !== 0) {
       setSubmited(true)
       return
     }
 
-    alert("Please, choose a note!")
-  }
+    alert("Please, choose a rate button!")
+ }
+
+
   return (
-  submited === false ? (
-    <div className="bg-gradient-dark text-white" mx-6 p-6 rounded-2xl font-overpass max-w-103>
-    <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
-      <img src={iconStar} alt="icon star" />
-    </div> 
-    <h1 className="text-2xl font-bold , mb- 2.5">How did we do?</h1>
+    submited === false ? (
+      <div className="bg-gradient-dark max-w-103 mx-6 p-6 rounded-2xl text-white font-overpass">
+        <div className="bg-dark-blue p-4 w-fit rounded-full  mb-4">
+          <img src={iconStar} alt="icon star" />
+        </div>
 
-    <p className="text-sm text-light-grey mb-6 leading-1">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-    <div className="flex justify-between mb-6">
-      <input type="button" value={1} className="bg-dark-blue w10.5 h-10 rounded full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm: w-13 sm:h-13 hover:bg-orange hover:text-white hover:cursor-pinter" onClick={() =>  handleMudarNotaAvaliacao(1)} />
-      <input type="button" value={2} className="bg-dark-blue w10.5 h-10 rounded full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white sm: w-13 sm:h-13  hover:bg-orange hover:text-white hover:cursor-pinter" onClick={() =>  handleMudarNotaAvaliacao(2)} />
-      <input type="button" value={3} className="bg-dark-blue w10.5 h-10 rounded full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white sm: w-13 sm:h-13  hover:bg-orange hover:text-white hover:cursor-pinter" onClick={() =>  handleMudarNotaAvaliacao(3)} />
-      <input type="button" value={4} className="bg-dark-blue w10.5 h-10 rounded full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white sm: w-13 sm:h-13  hover:bg-orange hover:text-white hover:cursor-pinter" onClick={() =>  handleMudarNotaAvaliacao(4)} />
-      <input type="button" value={5} className="bg-dark-blue w10.5 h-10 rounded full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white sm: w-13 sm:h-13  hover:bg-orange hover:text-white hover:cursor-pinter" onClick={() =>  handleMudarNotaAvaliacao(5)} />
-    </div>
-    <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange cursor-pointer">Submit</button>
+        <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
 
+        <p className="text-light-grey text-sm leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
-  </div>
-  ) : (
-    <div className= "bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
-      <img className ="mx-auto mb-6"src={illustrationThankYou} alt="illustrationThankYou" />
+        <div className="flex justify-between mb-6">
+          <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-600" onClick={() => handleAlterarNotaAvaliacao(1)} />
+          <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-600" onClick={() => handleAlterarNotaAvaliacao(2)} />
+          <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-600" onClick={() => handleAlterarNotaAvaliacao(3)} />
+          <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-600" onClick={() => handleAlterarNotaAvaliacao(4)} />
+          <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-600" onClick={() => handleAlterarNotaAvaliacao(5)} />
+        </div>
+
+        <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold hover:bg-white hover:text-orange cursor-pointer transition duration-600" onClick={handleSubmit}>Submit</button>
 
 
-      <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6">You selected {notaAvaliacao} out of 5</p>
+      </div>
+    ) : (
+      <div className="bg-gradient-dark max-w-103 mx-6 p-6 rounded-2xl text-white font-overpass text-center">
+        <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration Thank You" />
 
-      <h1 className="text-2xl font-bold , mb- 2.5 text-center">Thank You!</h1>
+        <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You selected {notaAvaliacao} out of 5!</p>
 
-      <p className="text-sm text-light-grey leading-1 text-center">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
-    </div>
+        <h1 className="text-2xl font-bold mb-2.5">Thank You!</h1>
 
-  )
+        <p className="text-light-grey text-sm leading-1">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+      </div>
+    )
   )
 }
-
